4975d2f8e989d75b0387938e605bf3eeb33064f7
Evan Meagher <evm@twitter.com>
2014-07-27 21:56:27 -0700

    Factor out connect msg construction, add some rustdocs.

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,9 @@ mod test;
 
 pub static DefaultSpreadPort: i16 = 4803;
 static MaxPrivateNameLength: uint = 10;
+static SpreadMajorVersion: u8 = 4;
+static SpreadMinorVersion: u8 = 4;
+static SpreadPatchVersion: u8 = 0;
 
 // Error codes, as per http://www.spread.org/docs/spread_docs_4/docs/error_codes.html
 pub enum SpreadError {
@@ -34,7 +37,7 @@ pub enum SpreadError {
     NetErrorOnSession = -18
 }
 
-pub struct SpreadConnection {
+pub struct SpreadClient {
     addr: SocketAddr,
     name: String,
     is_priority_connection: bool,
@@ -49,20 +52,19 @@ pub struct SpreadMessageHeader {
     recipients: Vec<SpreadGroup>
 }
 
-pub fn connect(
-    addr: SocketAddr,
+// Construct a byte vector representation of a connect message for the given
+// connection arguments.
+fn encode_connect_message(
     private_name: &str,
     is_priority_connection: bool,
     receive_membership_messages: bool
-) -> SpreadConnection {
-    // Send the initial connect message.
+) -> Vec<u8> {
     let mut buf: Vec<u8> = Vec::new();
 
     // Set Spread version.
-    // TODO: constants for elements.
-    buf.push(4);
-    buf.push(4);
-    buf.push(0);
+    buf.push(SpreadMajorVersion);
+    buf.push(SpreadMinorVersion);
+    buf.push(SpreadPatchVersion);
 
     // Apply masks for group membership and priority.
     let masked = match (is_priority_connection, receive_membership_messages) {
@@ -73,18 +75,45 @@ pub fn connect(
     };
     buf.push(masked);
 
+    buf.push(private_name.char_len() as u8);
+    buf.push_all(private_name.as_bytes());
+    buf
+}
+
+/// Establishes a named connection to a Spread daemon running at a given
+/// `SocketAddr`.
+///
+/// *Arguments:*
+///
+/// - `addr`: The address at which the Spread daemon is running.
+/// - `private_name`: A name to use privately to refer to the connection.
+/// - `is_priority_connection`: If true, indicates that the connection is
+///   prioritized.
+/// - `receive_membership_messages`: If true, membership messages will be
+///   received by the resultant client.
+pub fn connect(
+    addr: SocketAddr,
+    private_name: &str,
+    is_priority_connection: bool,
+    receive_membership_messages: bool
+) -> SpreadClient {
     // Truncate (if necessary) and write `private_name`.
     let truncated_private_name = match private_name {
         too_long if too_long.char_len() > MaxPrivateNameLength =>
             too_long.slice_to(MaxPrivateNameLength),
         just_fine => just_fine
     };
-    buf.push(truncated_private_name.char_len() as u8);
-    buf.push_all(truncated_private_name.as_bytes());
+
+    // Send the initial connect message.
+    let connect_message = encode_connect_message(
+        truncated_private_name,
+        is_priority_connection,
+        receive_membership_messages
+    );
 
     // TODO: Send the connect message.
 
-    SpreadConnection {
+    SpreadClient {
         addr: addr,
         name: String::from_str(truncated_private_name),
         is_priority_connection: is_priority_connection,
@@ -92,18 +121,23 @@ pub fn connect(
     }
 }
 
-impl SpreadConnection {
-    fn disconnect(&self) {
+impl SpreadClient {
+    /// Disconnects the connection to the Spread daemon.
+    pub fn disconnect(&self) {
         println!("{}", "disconnected");
     }
 
-    fn join(&self, group_name: &str) -> SpreadGroup {
+    /// Join a named Spread group on the client's connection.
+    /// All messages sent to the group will be received by the client until it
+    /// has left the group.
+    pub fn join(&self, group_name: &str) -> SpreadGroup {
         SpreadGroup {
             name: String::from_str(group_name)
         }
     }
 
-    fn multicast(
+    /// Send a message to all groups specified in the message header.
+    pub fn multicast(
         &self,
         data: &[u8],
         header: SpreadMessageHeader
--- src/test.rs
+++ src/test.rs
@@ -1,7 +1,14 @@
 #[cfg(test)]
 mod test {
     use std::io::net::ip::SocketAddr;
+    use encode_connect_message;
     use connect;
+
+    #[test]
+    fn should_encode_connect_message_with_sufficiently_short_private_name() {
+        let result = encode_connect_message("test", true, true);
+        assert_eq!(result, vec!(4, 4, 0, 17, 4, 116, 101, 115, 116));
+    }
 
     #[test]
     fn should_connect() {
